2399868168de98e71d3c3cfdf1d5c3d3784aeece
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-06-17 14:19:12 -0300

    Fix: loading resources from .local/share/fpakman/resources

--- fpakman/core/resource.py
+++ fpakman/core/resource.py
@@ -8,6 +8,6 @@ app_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def get_path(resource_path):
 
     if os.path.exists(local_resource_path):
-        return local_resource_path + resource_path
+        return local_resource_path + '/' + resource_path
     else:
         return app_dir + '/resources/' + resource_path
