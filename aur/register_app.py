ef3294a8c19e217f7f2622f34f13d344cf15f0cd
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-06-17 11:47:38 -0300

    Fix: AUR installation when '.local/share/applications' does not exist

--- aur/register_app.py
+++ aur/register_app.py
@@ -1,4 +1,5 @@
 # used for AUR installation
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -12,7 +13,12 @@ Exec = /usr/bin/fpakman
 Icon = /usr/lib/python{version}/site-packages/fpakman/resources/img/flathub_45.svg
 """.format(version="{}.{}".format(sys.version_info.major, sys.version_info.minor))
 
-file_path = '{}/.local/share/applications/fpakman.desktop'.format(str(Path.home()))
+apps_path = '{}/.local/share/applications'.format(str(Path.home()))
+
+if not os.path.exists(apps_path):
+    os.mkdir(apps_path)
+
+file_path = apps_path + '/fpakman.desktop'
 
 with open(file_path, 'w+') as f:
     f.write(desktop_file)
