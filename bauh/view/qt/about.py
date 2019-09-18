3ac8a08ad8427b726cc85c9a48de0e65739cdc4a
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-09-18 17:25:51 -0300

    higher fonts for about dialog

--- bauh/view/qt/about.py
+++ bauh/view/qt/about.py
@@ -23,14 +23,14 @@ class AboutDialog(QDialog):
         layout.addWidget(label_logo)
 
         label_name = QLabel('{} ( {} {} )'.format(__app_name__, locale_keys['version'].lower(), __version__))
-        label_name.setStyleSheet('font-weight: bold;')
+        label_name.setStyleSheet('font-weight: bold; font-size: 14px')
         label_name.setAlignment(Qt.AlignCenter)
         layout.addWidget(label_name)
 
         layout.addWidget(QLabel(''))
 
         line_desc = QLabel(self)
-        line_desc.setStyleSheet('font-size: 11px; font-weight: bold;')
+        line_desc.setStyleSheet('font-size: 12px; font-weight: bold;')
         line_desc.setText(locale_keys['about.info.desc'])
         line_desc.setAlignment(Qt.AlignCenter)
         line_desc.setMinimumWidth(400)
@@ -39,14 +39,14 @@ class AboutDialog(QDialog):
         layout.addWidget(QLabel(''))
 
         label_more_info = QLabel()
-        label_more_info.setStyleSheet('font-size: 10px;')
+        label_more_info.setStyleSheet('font-size: 11px;')
         label_more_info.setText(locale_keys['about.info.link'] + ": <a href='{url}'>{url}</a>".format(url=PROJECT_URL))
         label_more_info.setOpenExternalLinks(True)
         label_more_info.setAlignment(Qt.AlignCenter)
         layout.addWidget(label_more_info)
 
         label_license = QLabel()
-        label_license.setStyleSheet('font-size: 10px;')
+        label_license.setStyleSheet('font-size: 11px;')
         label_license.setText("<a href='{}'>{}</a>".format(LICENSE_URL, locale_keys['about.info.license']))
         label_license.setOpenExternalLinks(True)
         label_license.setAlignment(Qt.AlignCenter)
@@ -55,7 +55,7 @@ class AboutDialog(QDialog):
         layout.addWidget(QLabel(''))
 
         label_rate = QLabel()
-        label_rate.setStyleSheet('font-size: 10px; font-weight: bold;')
+        label_rate.setStyleSheet('font-size: 11px; font-weight: bold;')
         label_rate.setText(locale_keys['about.info.rate'] + ' :)')
         label_rate.setOpenExternalLinks(True)
         label_rate.setAlignment(Qt.AlignCenter)
