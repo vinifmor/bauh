b72590287d730e4f1921c7176f96fcdaa3539d18
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-09-20 14:59:21 -0300

    fix: AUR installation fails when gets warning messages from pacman

--- bauh/gems/arch/pacman.py
+++ bauh/gems/arch/pacman.py
@@ -99,7 +99,7 @@ def install_as_process(pkgpath: str, root_password: str, aur: bool, pkgdir: str 
     else:
         cmd = ['pacman', '-S', pkgpath, '--noconfirm']  # pkgpath = pkgname
 
-    return SystemProcess(new_root_subprocess(cmd, root_password, cwd=pkgdir), wrong_error_phrase='warning: downgrading package')
+    return SystemProcess(new_root_subprocess(cmd, root_password, cwd=pkgdir), wrong_error_phrase='warning:')
 
 
 def list_desktop_entries(pkgnames: Set[str]) -> List[str]:
