20517ad4793dd7046abb94cfacf92e067aed522f
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-10-20 14:45:59 -0300

    aur dist -> fix 'copy_icons' script

--- aur/copy_icons.py
+++ aur/copy_icons.py
@@ -6,7 +6,7 @@ from pathlib import Path
 output_base = sys.argv[2]
 
 for f in glob.glob(sys.argv[1] + '/*.png'):
-    res = f.split('/')[-1].split('.')
+    res = f.split('/')[-1].split('.')[0]
     dest_dir = output_base + '/' + res + '/apps'
 
     Path(dest_dir).mkdir(parents=True, exist_ok=True)
