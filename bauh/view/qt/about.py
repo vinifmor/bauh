8cd07aec2cf27fcdfeb4745396469441d8e3b8cc
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-11-27 17:29:28 -0300

    [about] minor changes

--- bauh/view/qt/about.py
+++ bauh/view/qt/about.py
@@ -20,7 +20,7 @@ class AboutDialog(QDialog):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        label_logo = QLabel(self)
+        label_logo = QLabel()
         label_logo.setPixmap(QPixmap(resource.get_path('img/logo.svg')))
         label_logo.setAlignment(Qt.AlignCenter)
         layout.addWidget(label_logo)
@@ -30,11 +30,15 @@ class AboutDialog(QDialog):
         label_name.setAlignment(Qt.AlignCenter)
         layout.addWidget(label_name)
 
+        label_version = QLabel(i18n['version'].lower() + ' ' + __version__)
+        label_version.setStyleSheet('QLabel { font-size: 10px; font-weight: bold }')
+        label_version.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label_version)
+
         layout.addWidget(QLabel(''))
 
-        line_desc = QLabel(self)
+        line_desc = QLabel(i18n['about.info.desc'])
         line_desc.setStyleSheet('font-size: 12px; font-weight: bold;')
-        line_desc.setText(i18n['about.info.desc'])
         line_desc.setAlignment(Qt.AlignCenter)
         line_desc.setMinimumWidth(400)
         layout.addWidget(line_desc)
@@ -56,14 +60,9 @@ class AboutDialog(QDialog):
         layout.addWidget(gems_widget)
         layout.addWidget(QLabel(''))
 
-        label_version = QLabel(i18n['version'].lower() + ' ' + __version__)
-        label_version.setStyleSheet('QLabel { font-size: 11px; font-weight: bold }')
-        label_version.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label_version)
-
         label_more_info = QLabel()
         label_more_info.setStyleSheet('font-size: 11px;')
-        label_more_info.setText(i18n['about.info.link'] + ": <a href='{url}'>{url}</a>".format(url=PROJECT_URL))
+        label_more_info.setText(i18n['about.info.link'] + " <a href='{url}'>{url}</a>".format(url=PROJECT_URL))
         label_more_info.setOpenExternalLinks(True)
         label_more_info.setAlignment(Qt.AlignCenter)
         layout.addWidget(label_more_info)
@@ -77,28 +76,29 @@ class AboutDialog(QDialog):
 
         layout.addWidget(QLabel(''))
 
-        label_rate = QLabel()
-        label_rate.setStyleSheet('font-size: 11px; font-weight: bold;')
-        label_rate.setText(i18n['about.info.rate'] + ' :)')
-        label_rate.setOpenExternalLinks(True)
-        label_rate.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label_rate)
+        label_trouble_question = QLabel(i18n['about.info.trouble.question'])
+        label_trouble_question.setStyleSheet('font-size: 10px; font-weight: bold')
+        label_trouble_question.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(label_trouble_question)
+
+        label_trouble_answer = QLabel(i18n['about.info.trouble.answer'])
+        label_trouble_answer.setStyleSheet('font-size: 10px;')
+        label_trouble_answer.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(label_trouble_answer)
 
         layout.addWidget(QLabel(''))
 
-        label_trouble_question = QLabel()
-        label_trouble_question.setStyleSheet('font-size: 10px;')
-        label_trouble_question.setText(i18n['about.info.trouble.question'])
-        label_trouble_question.setOpenExternalLinks(True)
-        label_trouble_question.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label_trouble_question)
+        label_rate_question = QLabel(i18n['about.info.rate.question'])
+        label_rate_question.setStyleSheet('font-size: 10px; font-weight: bold;')
+        label_rate_question.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label_rate_question)
 
-        label_trouble_body = QLabel()
-        label_trouble_body.setStyleSheet('font-size: 10px;')
-        label_trouble_body.setText(i18n['about.info.trouble.body'])
-        label_trouble_body.setOpenExternalLinks(True)
-        label_trouble_body.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label_trouble_body)
+        label_rate_answer = QLabel(i18n['about.info.rate.answer'])
+        label_rate_answer.setStyleSheet('font-size: 10px;')
+        label_rate_answer.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label_rate_answer)
 
         layout.addWidget(QLabel(''))
 
