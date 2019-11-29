a9da7bacc2791aa8db652c21611a36c259dc565a
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2019-11-29 09:20:11 -0300

    [aur] new suggestion

--- bauh/gems/arch/suggestions.py
+++ bauh/gems/arch/suggestions.py
@@ -2,4 +2,5 @@ from bauh.api.abstract.model import SuggestionPriority
 
 ALL = {'google-chrome': SuggestionPriority.HIGH,
        'lutris-git': SuggestionPriority.LOW,
-       'xdman': SuggestionPriority.LOW}
+       'xdman': SuggestionPriority.LOW,
+       'optimus-manager-qt': SuggestionPriority.LOW}
