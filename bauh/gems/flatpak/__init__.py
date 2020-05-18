85ca764da1bfcf43ed015582994a7aee0957d480
Brorean <Brorean@users.noreply.github.com>
2020-05-18 12:11:24 -0500

    Update __init__.py
    Add get_icon_path

--- bauh/gems/flatpak/__init__.py
+++ bauh/gems/flatpak/__init__.py
@@ -1,6 +1,11 @@
 import os
 from pathlib import Path
 
+from bauh.commons import resource
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SUGGESTIONS_FILE = 'https://raw.githubusercontent.com/vinifmor/bauh-files/master/flatpak/suggestions.txt'
 CONFIG_FILE = '{}/.config/bauh/flatpak.yml'.format(Path.home())
+
+def get_icon_path() -> str:
+    return resource.get_path('img/flatpak.svg', ROOT_DIR)
