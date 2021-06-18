vinifmor/bauh

commit db5eeabfb89a972fe85f42fb3aa838eda46c348b

[arch] fix: some dependencies not being matched during comparisons between numeric and alphanumeric versions

bauh/gems/arch/dependencies.py
@@ -3,13 +3,11 @@ import traceback
 from threading import Thread
 from typing import Set, List, Tuple, Dict, Iterable, Optional
 
-from packaging.version import parse as parse_version
-
 from bauh.api.abstract.handler import ProcessWatcher
 from bauh.gems.arch import pacman, message, sorting, confirmation
 from bauh.gems.arch.aur import AURClient
 from bauh.gems.arch.exceptions import PackageNotFoundException
-from bauh.gems.arch.util import clean_version
+from bauh.gems.arch.version import compare_versions
 from bauh.view.util.translation import I18n
 
 
@@ -220,7 +218,7 @@ class DependenciesAnalyser:
                     matched_providers = set()
                     split_informed_dep = self.re_dep_operator.split(dep_exp)
                     try:
-                        version_required = parse_version(clean_version(split_informed_dep[2]))
+                        version_required = split_informed_dep[2]
                         exp_op = split_informed_dep[1] if split_informed_dep[1] != '=' else '=='
 
                         for p in providers:
@@ -230,9 +228,9 @@ class DependenciesAnalyser:
                                 split_dep = self.re_dep_operator.split(provided_exp)
 
                                 if len(split_dep) == 3 and split_dep[0] == dep_name:
-                                    version_provided = parse_version(clean_version(split_dep[2]))
+                                    version_provided = split_dep[2]
 
-                                    if eval('version_provided {} version_required'.format(exp_op)):
+                                    if compare_versions(version_provided, exp_op, version_required):
                                         matched_providers.add(p)
                                         break
 
@@ -270,17 +268,17 @@ class DependenciesAnalyser:
                     self.__raise_dependency_not_found(dep_exp, watcher)
                 else:
                     try:
-                        dep_version = parse_version(clean_version(dep_info[0]['Version']))
+                        dep_version = dep_info[0]['Version']
                     except:
                         traceback.print_exc()
-                        self.__raise_dependency_not_found(dep_exp, watcher)
+                        return self.__raise_dependency_not_found(dep_exp, watcher)
 
                     split_informed_dep = self.re_dep_operator.split(dep_exp)
                     try:
-                        version_required = parse_version(clean_version(split_informed_dep[2]))
+                        version_required = split_informed_dep[2]
                         exp_op = split_informed_dep[1] if split_informed_dep[1] != '=' else '=='
 
-                        if eval('dep_version {} version_required'.format(exp_op)):
+                        if compare_versions(dep_version, exp_op, version_required):
                             aur_deps.add(dep_name)
                             missing_deps.add((dep_name, 'aur'))
                     except:
@@ -334,20 +332,11 @@ class DependenciesAnalyser:
                                 version_found = [p for p in provided_map if p.startswith(version_pattern)]
 
                                 if version_found:
-                                    version_found = clean_version(version_found[0].split('=')[1])
-                                    version_required = clean_version(dep_split[2])
+                                    version_found = version_found[0].split('=')[1]
+                                    version_required = dep_split[2]
+                                    op = dep_split[1] if dep_split[1] != '=' else '=='
 
-                                    try:
-                                        version_found = parse_version(version_found)
-                                        version_required = parse_version(version_required)
-
-                                        op = dep_split[1] if dep_split[1] != '=' else '=='
-                                        match = eval('version_found {} version_required'.format(op))
-                                    except:
-                                        match = False
-                                        traceback.print_exc()
-
-                                    if not match:
+                                    if not compare_versions(version_found, op, version_required):
                                         self._fill_missing_dep(dep_name=dep_name, dep_exp=dep, aur_index=aur_index,
                                                                missing_deps=missing_deps,
                                                                remote_provided_map=remote_provided_map,

bauh/gems/arch/mapper.py
@@ -1,14 +1,11 @@
 import logging
 import os
-import traceback
 from typing import Optional
-
-from colorama import Fore
-from packaging.version import parse as parse_version
 
 from bauh.api.abstract.model import PackageStatus
 from bauh.api.http import HttpClient
 from bauh.gems.arch.model import ArchPackage
+from bauh.gems.arch.version import compare_versions
 from bauh.view.util.translation import I18n
 
 URL_PKG_DOWNLOAD = 'https://aur.archlinux.org/{}'
@@ -64,26 +61,7 @@ class AURDataMapper:
     @staticmethod
     def check_version_update(version: str, latest_version: str) -> bool:
         if version and latest_version and version != latest_version:
-            try:
-                ver_epoch, latest_epoch = version.split(':'), latest_version.split(':')
-
-                if len(ver_epoch) > 1 and len(latest_epoch) > 1:
-                    parsed_ver_epoch, parsed_latest_epoch = parse_version(ver_epoch[0]), parse_version(latest_epoch[0])
-
-                    if parsed_ver_epoch == parsed_latest_epoch:
-                        return parse_version(''.join(ver_epoch[1:])) < parse_version(''.join(latest_epoch[1:]))
-                    else:
-                        return parsed_ver_epoch < parsed_latest_epoch
-                elif len(ver_epoch) > 1 and len(latest_epoch) == 1:
-                    return False
-                elif len(ver_epoch) == 1 and len(latest_epoch) > 1:
-                    return True
-                else:
-                    return parse_version(version) < parse_version(latest_version)
-            except:
-                print('{}Version: {}. Latest version: {}{}'.format(Fore.RED, version, latest_version, Fore.RESET))
-                traceback.print_exc()
-                return False
+            return compare_versions(latest_version, '>', version)
 
         return False
 

bauh/gems/arch/updates.py
@@ -4,8 +4,6 @@ import traceback
 from threading import Thread
 from typing import Dict, Set, List, Tuple, Iterable, Optional
 
-from packaging.version import parse as parse_version
-
 from bauh.api.abstract.controller import UpgradeRequirements, UpgradeRequirement
 from bauh.api.abstract.handler import ProcessWatcher
 from bauh.gems.arch import pacman, sorting
@@ -14,6 +12,7 @@ from bauh.gems.arch.dependencies import DependenciesAnalyser
 from bauh.gems.arch.exceptions import PackageNotFoundException
 from bauh.gems.arch.model import ArchPackage
 from bauh.gems.arch.pacman import RE_DEP_OPERATORS
+from bauh.gems.arch.version import compare_versions
 from bauh.view.util.translation import I18n
 
 
@@ -733,9 +732,7 @@ class UpdatesSummarizer:
 
                             for v in versions:
                                 try:
-                                    provided_version, required_version = parse_version(v), parse_version(dep_split[1])
-
-                                    if eval('provided_version {} required_version'.format(op)):
+                                    if compare_versions(v, op, dep_split[1]):
                                         version_match = True
                                         break
                                 except:

bauh/gems/arch/version.py
@@ -1,0 +1,35 @@
+from typing import Optional
+
+from packaging.version import parse
+
+
+def normalize_version(version: str) -> Optional[str]:
+    if version:
+        final_version = version.strip()
+
+        if final_version:
+            if ':' not in final_version:
+                final_version = '0:{}'.format(final_version)
+
+            if '-' not in final_version:
+                final_version = '{}-1'.format(final_version)
+
+        return final_version
+
+
+def compare_versions(version_a: str, operator: str, version_b: str) -> bool:
+    if operator:
+        a_obj, b_obj = parse(normalize_version(version_a)), parse(normalize_version(version_b))
+
+        if operator == '>':
+            return a_obj > b_obj
+        elif operator == '>=':
+            return a_obj >= b_obj
+        elif operator == '<':
+            return a_obj < b_obj
+        elif operator == '<=':
+            return a_obj <= b_obj
+        elif operator == '==':
+            return a_obj == b_obj
+
+    raise Exception("compare: invalid string operator {}".format(operator))

tests/gems/arch/test_version.py
@@ -1,0 +1,46 @@
+from unittest import TestCase
+
+from bauh.gems.arch import version
+
+
+class CompareVersionsTest(TestCase):
+
+    def test_both_versions_only_filled_with_numbers_and_same_number_of_divisions(self):
+        self.assertFalse(version.compare_versions('1', '>', '2'))
+        self.assertTrue(version.compare_versions('1', '<', '2'))
+        self.assertTrue(version.compare_versions('1', '<=', '2'))
+        self.assertTrue(version.compare_versions('2', '>=', '1'))
+        self.assertTrue(version.compare_versions('1', '==', '1'))
+        self.assertFalse(version.compare_versions('1.1', '>', '2.0'))
+        self.assertTrue(version.compare_versions('1.1', '<', '2.0'))
+        self.assertTrue(version.compare_versions('1.1', '<=', '2.0'))
+        self.assertTrue(version.compare_versions('2.1', '>', '2.0'))
+        self.assertTrue(version.compare_versions('2.1', '>=', '2.0'))
+        self.assertTrue(version.compare_versions('2.0', '==', '2.0'))
+
+    def test_versions_only_filled_with_numbers_and_different_number_of_divisons(self):
+        self.assertFalse(version.compare_versions('1.0', '>', '2'))
+        self.assertTrue(version.compare_versions('1', '<', '2.0'))
+        self.assertTrue(version.compare_versions('1.1.1', '<=', '2'))
+        self.assertTrue(version.compare_versions('2', '>=', '1.0'))
+        self.assertTrue(version.compare_versions('1.0', '==', '1'))
+        self.assertTrue(version.compare_versions('1.1', '>', '1'))
+        self.assertFalse(version.compare_versions('1.1', '<', '1'))
+
+    def test_one_version_contain_mixed_letters_and_symbols(self):
+        self.assertTrue(version.compare_versions('2.3.3op2', '>=', '2.2.6'))
+        self.assertTrue(version.compare_versions('2.2a', '<', '2.2b'))
+        self.assertTrue(version.compare_versions('2.2a', '<', '2.3a'))
+
+    def test_versions_with_epochs(self):
+        self.assertTrue(version.compare_versions('1:1', '==', '1:1.0'))
+        self.assertTrue(version.compare_versions('1:1', '<', '1:1.1'))
+        self.assertTrue(version.compare_versions('1:1', '<=', '1:1.1'))
+        self.assertTrue(version.compare_versions('2:0', '>', '1:1.1'))
+        self.assertTrue(version.compare_versions('1:0', '>', '2.0-1'))
+
+    def test_versions_with_release_number(self):
+        self.assertTrue(version.compare_versions('1.0-1', '<', '1.0-2'))
+        self.assertTrue(version.compare_versions('1.0-2', '>', '1.0-1'))
+        self.assertTrue(version.compare_versions('1.0-1', '<', '1.0-10'))
+        self.assertTrue(version.compare_versions('1.0-10', '>', '1.0-1'))

bauh/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.9.17'
+__version__ = '0.9.18'
 __app_name__ = 'bauh'
 
 import os

commit c2493befda2493cda5d671ad7777d36646ad85e9

[arch] fix -> regressions from commit db5eeabf

bauh/gems/arch/dependencies.py
@@ -7,7 +7,7 @@ from bauh.api.abstract.handler import ProcessWatcher
 from bauh.gems.arch import pacman, message, sorting, confirmation
 from bauh.gems.arch.aur import AURClient
 from bauh.gems.arch.exceptions import PackageNotFoundException
-from bauh.gems.arch.version import compare_versions
+from bauh.gems.arch.version import match_required_version
 from bauh.view.util.translation import I18n
 
 
@@ -229,7 +229,7 @@ class DependenciesAnalyser:
                                 if len(split_dep) == 3 and split_dep[0] == dep_name:
                                     version_provided = split_dep[2]
 
-                                    if compare_versions(version_provided, exp_op, version_required):
+                                    if match_required_version(version_provided, exp_op, version_required):
                                         matched_providers.add(p)
                                         break
 
@@ -275,9 +275,9 @@ class DependenciesAnalyser:
                     split_informed_dep = self.re_dep_operator.split(dep_exp)
                     try:
                         version_required = split_informed_dep[2]
-                        exp_op = split_informed_dep[1] if split_informed_dep[1] != '=' else '=='
+                        exp_op = split_informed_dep[1].strip()
 
-                        if compare_versions(dep_version, exp_op, version_required):
+                        if match_required_version(dep_version, exp_op, version_required):
                             aur_deps.add(dep_name)
                             missing_deps.add((dep_name, 'aur'))
                     except:
@@ -333,9 +333,9 @@ class DependenciesAnalyser:
                                 if version_found:
                                     version_found = version_found[0].split('=')[1]
                                     version_required = dep_split[2]
-                                    op = dep_split[1] if dep_split[1] != '=' else '=='
+                                    op = dep_split[1].strip()
 
-                                    if not compare_versions(version_found, op, version_required):
+                                    if not match_required_version(version_found, op, version_required):
                                         self._fill_missing_dep(dep_name=dep_name, dep_exp=dep, aur_index=aur_index,
                                                                missing_deps=missing_deps,
                                                                remote_provided_map=remote_provided_map,

bauh/gems/arch/mapper.py
@@ -5,7 +5,7 @@ from typing import Optional
 from bauh.api.abstract.model import PackageStatus
 from bauh.api.http import HttpClient
 from bauh.gems.arch.model import ArchPackage
-from bauh.gems.arch.version import compare_versions
+from bauh.gems.arch.version import normalize_version
 from bauh.view.util.translation import I18n
 
 URL_PKG_DOWNLOAD = 'https://aur.archlinux.org/{}'
@@ -61,7 +61,7 @@ class AURDataMapper:
     @staticmethod
     def check_version_update(version: str, latest_version: str) -> bool:
         if version and latest_version and version != latest_version:
-            return compare_versions(latest_version, '>', version)
+            return normalize_version(latest_version) > normalize_version(version)
 
         return False
 

bauh/gems/arch/updates.py
@@ -12,7 +12,7 @@ from bauh.gems.arch.dependencies import DependenciesAnalyser
 from bauh.gems.arch.exceptions import PackageNotFoundException
 from bauh.gems.arch.model import ArchPackage
 from bauh.gems.arch.pacman import RE_DEP_OPERATORS
-from bauh.gems.arch.version import compare_versions
+from bauh.gems.arch.version import match_required_version
 from bauh.view.util.translation import I18n
 
 
@@ -725,14 +725,11 @@ class UpdatesSummarizer:
                         if versions:
                             op = ''.join(RE_DEP_OPERATORS.findall(dep))
 
-                            if op == '=':
-                                op = '=='
-
                             version_match = False
 
                             for v in versions:
                                 try:
-                                    if compare_versions(v, op, dep_split[1]):
+                                    if match_required_version(current_version=v, operator=op, required_version=dep_split[1]):
                                         version_match = True
                                         break
                                 except:

bauh/gems/arch/version.py
@@ -1,35 +1,51 @@
-from typing import Optional
+import re
 
-from packaging.version import parse
+from packaging.version import LegacyVersion
 
-
-def normalize_version(version: str) -> Optional[str]:
-    if version:
-        final_version = version.strip()
-
-        if final_version:
-            if ':' not in final_version:
-                final_version = '0:{}'.format(final_version)
-
-            if '-' not in final_version:
-                final_version = '{}-1'.format(final_version)
-
-        return final_version
+RE_VERSION_WITH_RELEASE = re.compile(r'^(.+)-\d+$')
+RE_VERSION_WITH_EPOCH = re.compile(r'^\d+:(.+)$')
 
 
-def compare_versions(version_a: str, operator: str, version_b: str) -> bool:
-    if operator:
-        a_obj, b_obj = parse(normalize_version(version_a)), parse(normalize_version(version_b))
+def normalize_version(version: str) -> LegacyVersion:
+    final_version = version.strip()
 
-        if operator == '>':
-            return a_obj > b_obj
-        elif operator == '>=':
-            return a_obj >= b_obj
-        elif operator == '<':
-            return a_obj < b_obj
-        elif operator == '<=':
-            return a_obj <= b_obj
-        elif operator == '==':
-            return a_obj == b_obj
+    if not RE_VERSION_WITH_EPOCH.match(final_version):
+        final_version = '0:{}'.format(final_version)
 
-    raise Exception("compare: invalid string operator {}".format(operator))
+    if not RE_VERSION_WITH_RELEASE.match(final_version):
+        final_version = '{}-1'.format(final_version)
+
+    return LegacyVersion(final_version)
+
+
+def match_required_version(current_version: str, operator: str, required_version: str) -> bool:
+    final_required, final_current = required_version.strip(), current_version.strip()
+
+    required_has_epoch, current_no_epoch = bool(RE_VERSION_WITH_EPOCH.match(final_required)), RE_VERSION_WITH_EPOCH.split(final_current)
+    current_has_epoch = len(current_no_epoch) > 1
+
+    if required_has_epoch and not current_has_epoch:
+        final_current = '0:{}'.format(final_current)
+    elif current_has_epoch and not required_has_epoch:
+        final_current = current_no_epoch[1]
+
+    required_has_release, current_no_release = bool(RE_VERSION_WITH_RELEASE.match(final_required)), RE_VERSION_WITH_RELEASE.split(final_current)
+    current_has_release = len(current_no_release) > 1
+
+    if required_has_release and not current_has_release:
+        final_current = '{}-1'.format(final_current)
+    elif current_has_release and not required_has_release:
+        final_current = current_no_release[1]
+
+    final_required, final_current = LegacyVersion(final_required), LegacyVersion(final_current)
+
+    if operator == '==' or operator == '=':
+        return final_current == final_required
+    elif operator == '>':
+        return final_current > final_required
+    elif operator == '>=':
+        return final_current >= final_required
+    elif operator == '<':
+        return final_current < final_required
+    elif operator == '<=':
+        return final_current <= final_required

tests/gems/arch/test_aur_data_mapper.py
@@ -10,7 +10,7 @@ class ArchDataMapperTest(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        warnings.filterwarnings('ignore')
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
 
     def test_check_version_update(self):
         self.assertTrue(AURDataMapper.check_version_update('1.0.0-1', '1.0.0-2'))
@@ -181,3 +181,17 @@ class ArchDataMapperTest(TestCase):
         # in this case, install_date will be considered instead of package's last_modified.
         # as 'install_date' is higher, only the string versions will be compared
         self.assertTrue(mapper.check_update(pkg=pkg, last_modified=None))
+
+    def test_check_version_update__one_version_contain_mixed_letters_and_symbols(self):
+        self.assertTrue(AURDataMapper.check_version_update('2.2.6', '2.3.3op2'))
+        self.assertFalse(AURDataMapper.check_version_update('2.3', '2.2.a'))
+        self.assertTrue(AURDataMapper.check_version_update('2.2.a.123', '2.3'))
+
+    def test_check_update__only_installed_version_with_release_number(self):
+        self.assertTrue(AURDataMapper.check_version_update('2.2.6-1', '2.3'))
+        self.assertTrue(AURDataMapper.check_version_update('2.2', '2.2-2'))
+        self.assertFalse(AURDataMapper.check_version_update('2.2', '2.2-1'))
+
+    def test_check_update__versions_with_epoch(self):
+        self.assertTrue(AURDataMapper.check_version_update('2.3', '1:2.1'))
+        self.assertFalse(AURDataMapper.check_version_update('1:1.0', '2.1'))

tests/gems/arch/test_version.py
@@ -4,48 +4,55 @@ from unittest import TestCase
 from bauh.gems.arch import version
 
 
-class CompareVersionsTest(TestCase):
+class MatchRequiredVersionTest(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        warnings.filterwarnings('ignore')
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
 
-    def test_both_versions_only_filled_with_numbers_and_same_number_of_divisions(self):
-        self.assertFalse(version.compare_versions('1', '>', '2'))
-        self.assertTrue(version.compare_versions('1', '<', '2'))
-        self.assertTrue(version.compare_versions('1', '<=', '2'))
-        self.assertTrue(version.compare_versions('2', '>=', '1'))
-        self.assertTrue(version.compare_versions('1', '==', '1'))
-        self.assertFalse(version.compare_versions('1.1', '>', '2.0'))
-        self.assertTrue(version.compare_versions('1.1', '<', '2.0'))
-        self.assertTrue(version.compare_versions('1.1', '<=', '2.0'))
-        self.assertTrue(version.compare_versions('2.1', '>', '2.0'))
-        self.assertTrue(version.compare_versions('2.1', '>=', '2.0'))
-        self.assertTrue(version.compare_versions('2.0', '==', '2.0'))
+    def test_must_accept_single_or_both_equal_symbols_as_valid(self):
+        self.assertTrue(version.match_required_version('1', '=', '1'))
+        self.assertTrue(version.match_required_version('1', '==', '1'))
 
-    def test_versions_only_filled_with_numbers_and_different_number_of_divisons(self):
-        self.assertFalse(version.compare_versions('1.0', '>', '2'))
-        self.assertTrue(version.compare_versions('1', '<', '2.0'))
-        self.assertTrue(version.compare_versions('1.1.1', '<=', '2'))
-        self.assertTrue(version.compare_versions('2', '>=', '1.0'))
-        self.assertTrue(version.compare_versions('1.0', '==', '1'))
-        self.assertTrue(version.compare_versions('1.1', '>', '1'))
-        self.assertFalse(version.compare_versions('1.1', '<', '1'))
+    def test_must_ignore_release_number_when_required_has_no_defined(self):
+        self.assertTrue(version.match_required_version('1.3.0-2', '==', '1.3.0'))
+        self.assertTrue(version.match_required_version('1.3.0-2', '<=', '1.3.0'))
+        self.assertTrue(version.match_required_version('1.3.0-2', '>=', '1.3.0'))
+        self.assertFalse(version.match_required_version('1.3.0-2', '<', '1.3.0'))
+        self.assertFalse(version.match_required_version('1.3.0-2', '>', '1.3.0'))
 
-    def test_one_version_contain_mixed_letters_and_symbols(self):
-        self.assertTrue(version.compare_versions('2.3.3op2', '>=', '2.2.6'))
-        self.assertTrue(version.compare_versions('2.2a', '<', '2.2b'))
-        self.assertTrue(version.compare_versions('2.2a', '<', '2.3a'))
+    def test_must_ignore_epoch_number_when_required_has_no_defined(self):
+        self.assertTrue(version.match_required_version('1:1.3.0-1', '>', '1.1.0'))
+        self.assertTrue(version.match_required_version('1:1.3.0-1', '>=', '1.1.0'))
+        self.assertFalse(version.match_required_version('1:1.3.0-1', '<', '1.1.0'))
+        self.assertFalse(version.match_required_version('1:1.3.0-1', '<=', '1.1.0'))
+        self.assertFalse(version.match_required_version('1:1.3.0-1', '==', '1.1.0'))
 
-    def test_versions_with_epochs(self):
-        self.assertTrue(version.compare_versions('1:1', '==', '1:1.0'))
-        self.assertTrue(version.compare_versions('1:1', '<', '1:1.1'))
-        self.assertTrue(version.compare_versions('1:1', '<=', '1:1.1'))
-        self.assertTrue(version.compare_versions('2:0', '>', '1:1.1'))
-        self.assertTrue(version.compare_versions('1:0', '>', '2.0-1'))
+    def test_must_consider_default_epoch_for_current_version_when_required_has_epoch(self):
+        self.assertFalse(version.match_required_version('1.1.0', '==', '1:1.1.0'))  # 1.1.0 -> 0:1.1.0
+        self.assertFalse(version.match_required_version('1.1.0', '>', '1:1.1.0'))  # 1.1.0 -> 0:1.1.0
+        self.assertFalse(version.match_required_version('1.1.0', '>=', '1:1.1.0'))  # 1.1.0 -> 0:1.1.0
+        self.assertTrue(version.match_required_version('1.1.0', '<', '1:1.1.0'))  # 1.1.0 -> 0:1.1.0
+        self.assertTrue(version.match_required_version('1.1.0', '<=', '1:1.1.0'))  # 1.1.0 -> 0:1.1.0
 
-    def test_versions_with_release_number(self):
-        self.assertTrue(version.compare_versions('1.0-1', '<', '1.0-2'))
-        self.assertTrue(version.compare_versions('1.0-2', '>', '1.0-1'))
-        self.assertTrue(version.compare_versions('1.0-1', '<', '1.0-10'))
-        self.assertTrue(version.compare_versions('1.0-10', '>', '1.0-1'))
+        self.assertTrue(version.match_required_version('1.1.0', '==', '0:1.1.0'))  # 1.1.0 -> 0:1.1.0
+
+    def test_must_match_when_current_version_is_composed_of_alphanumerics_but_required_no(self):
+        self.assertTrue(version.match_required_version('2.3.3op2', '>', '2.2.6'))
+        self.assertTrue(version.match_required_version('2.3.3op2', '>=', '2.2.6'))
+        self.assertFalse(version.match_required_version('2.3.3op2', '==', '2.2.6'))
+        self.assertFalse(version.match_required_version('2.3.3op2', '<', '2.2.6'))
+        self.assertFalse(version.match_required_version('2.3.3op2', '<=', '2.2.6'))
+
+        # opposite comparisons
+        self.assertFalse(version.match_required_version('2.2.6', '>', '2.3.3op2'))
+        self.assertFalse(version.match_required_version('2.2.6', '>=', '2.3.3op2'))
+        self.assertFalse(version.match_required_version('2.2.6', '==', '2.3.3op2'))
+        self.assertTrue(version.match_required_version('2.2.6', '<', '2.3.3op2'))
+        self.assertTrue(version.match_required_version('2.2.6', '<=', '2.3.3op2'))
+
+    def test_must_match_when_both_versions_are_composed_of_alphanumerics(self):
+        self.assertTrue(version.match_required_version('2.3.3ab', '==', '2.3.3ab'))
+        self.assertTrue(version.match_required_version('2.3.3ab', '<', '2.3.3ac'))
+        self.assertTrue(version.match_required_version('2.3.3ad', '>', '2.3.3ac'))
+        self.assertTrue(version.match_required_version('2.3.3ad', '<', '2.3.3ad.1'))