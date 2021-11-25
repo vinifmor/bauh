7265c82fe52267f552d9c9585d2dbb68e18ec7ff
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2021-11-25 16:25:57 -0300

    [web.__init__] refactoring: String formatting method

--- bauh/gems/web/__init__.py
+++ bauh/gems/web/__init__.py
@@ -1,29 +1,30 @@
 import os
 from pathlib import Path
 
+from bauh import __app_name__
 from bauh.api.paths import DESKTOP_ENTRIES_DIR, CONFIG_DIR, TEMP_DIR, CACHE_DIR
 from bauh.commons import resource
 from bauh.commons.util import map_timestamp_file
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-WEB_PATH = '{}/.local/share/bauh/web'.format(Path.home())
+WEB_PATH = f'{Path.home()}/.local/share/{__app_name__}/web'
 WEB_CACHE_DIR = f'{CACHE_DIR}/web'
-INSTALLED_PATH = '{}/installed'.format(WEB_PATH)
-ENV_PATH = '{}/env'.format(WEB_PATH)
-FIXES_PATH = '{}/fixes'.format(WEB_PATH)
-NODE_DIR_PATH = '{}/node'.format(ENV_PATH)
-NODE_PATHS = {NODE_DIR_PATH + '/bin'}
-NODE_BIN_PATH = '{}/bin/node'.format(NODE_DIR_PATH)
-NPM_BIN_PATH = '{}/bin/npm'.format(NODE_DIR_PATH)
-NODE_MODULES_PATH = '{}/node_modules'.format(ENV_PATH)
-NATIVEFIER_BIN_PATH = '{}/.bin/nativefier'.format(NODE_MODULES_PATH)
+INSTALLED_PATH = f'{WEB_PATH}/installed'
+ENV_PATH = f'{WEB_PATH}/env'
+FIXES_PATH = f'{WEB_PATH}/fixes'
+NODE_DIR_PATH = f'{ENV_PATH}/node'
+NODE_PATHS = {f'{NODE_DIR_PATH}/bin'}
+NODE_BIN_PATH = f'{NODE_DIR_PATH}/bin/node'
+NPM_BIN_PATH = f'{NODE_DIR_PATH}/bin/npm'
+NODE_MODULES_PATH = f'{ENV_PATH}/node_modules'
+NATIVEFIER_BIN_PATH = f'{NODE_MODULES_PATH}/.bin/nativefier'
 ELECTRON_CACHE_DIR = f'{ENV_PATH}/electron'
 ELECTRON_DOWNLOAD_URL = 'https://github.com/electron/electron/releases/download/v{version}/electron-v{version}-linux-{arch}.zip'
 ELECTRON_SHA256_URL = 'https://github.com/electron/electron/releases/download/v{version}/SHASUMS256.txt'
 ELECTRON_WIDEVINE_URL = 'https://github.com/castlabs/electron-releases/releases/download/v{version}-wvvmp/electron-v{version}-wvvmp-linux-{arch}.zip'
 ELECTRON_WIDEVINE_SHA256_URL = 'https://github.com/castlabs/electron-releases/releases/download/v{version}-wvvmp/SHASUMS256.txt'
-URL_ENVIRONMENT_SETTINGS = 'https://raw.githubusercontent.com/vinifmor/bauh-files/master/web/env/v1/environment.yml'
-DESKTOP_ENTRY_PATH_PATTERN = DESKTOP_ENTRIES_DIR + '/bauh.web.{name}.desktop'
+URL_ENVIRONMENT_SETTINGS = f'https://raw.githubusercontent.com/vinifmor/{__app_name__}-files/master/web/env/v1/environment.yml'
+DESKTOP_ENTRY_PATH_PATTERN = f'{DESKTOP_ENTRIES_DIR}/{__app_name__}.web.' + '{name}.desktop'
 URL_FIX_PATTERN = "https://raw.githubusercontent.com/vinifmor/bauh-files/master/web/fix/{url}.js"
 URL_SUGGESTIONS = "https://raw.githubusercontent.com/vinifmor/bauh-files/master/web/env/v1/suggestions.yml"
 UA_CHROME = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
