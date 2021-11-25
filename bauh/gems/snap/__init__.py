9287e1176d93369b206da32c3c34a2175b17a7b0
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2021-11-25 16:16:15 -0300

    [snap] refactoring: SNAP_CACHE_PATH renamed to SNAP_CACHE_DIR

--- bauh/gems/snap/__init__.py
+++ bauh/gems/snap/__init__.py
@@ -4,9 +4,9 @@ from bauh.api.paths import CONFIG_DIR, CACHE_DIR
 from bauh.commons import resource
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-SNAP_CACHE_PATH = f'{CACHE_DIR}/snap'
+SNAP_CACHE_DIR = f'{CACHE_DIR}/snap'
 CONFIG_FILE = f'{CONFIG_DIR}/snap.yml'
-CATEGORIES_FILE_PATH = f'{SNAP_CACHE_PATH}/categories.txt'
+CATEGORIES_FILE_PATH = f'{SNAP_CACHE_DIR}/categories.txt'
 URL_CATEGORIES_FILE = 'https://raw.githubusercontent.com/vinifmor/bauh-files/master/snap/categories.txt'
 SUGGESTIONS_FILE = 'https://raw.githubusercontent.com/vinifmor/bauh-files/master/snap/suggestions.txt'
 
