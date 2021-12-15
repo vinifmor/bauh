388191d633cca52a6be36a59d99d66a59f83ff82
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2021-12-15 13:52:46 -0300

    [web] improvement: adding a placeholder to the address field on the installation form

--- bauh/gems/web/controller.py
+++ bauh/gems/web/controller.py
@@ -485,7 +485,8 @@ class WebApplicationManager(SoftwareManager):
     def _ask_install_options(self, app: WebApplication, watcher: ProcessWatcher, pre_validated: bool) -> Tuple[bool, List[str]]:
         watcher.change_substatus(self.i18n['web.install.substatus.options'])
 
-        inp_url = TextInputComponent(label=self.i18n['address'].capitalize() + ' (URL)', capitalize_label=False, value=app.url, read_only=pre_validated)
+        inp_url = TextInputComponent(label=self.i18n['address'].capitalize() + ' (URL)', capitalize_label=False, value=app.url,
+                                     read_only=pre_validated, placeholder=f"({self.i18n['example.short']}: https://myapp123.com)")
         inp_name = TextInputComponent(label=self.i18n['name'], value=app.name)
         inp_desc = TextInputComponent(label=self.i18n['description'], value=app.description)
 
