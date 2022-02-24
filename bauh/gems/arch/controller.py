23bc65b00ed3e4f6369ed4738b398a385f51ed29
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2022-02-24 09:54:29 -0300

    [arch] refactoring: removing unneeded parameters from 'launch' method

--- bauh/gems/arch/controller.py
+++ bauh/gems/arch/controller.py
@@ -2781,7 +2781,7 @@ class ArchManager(SoftwareManager):
     def launch(self, pkg: ArchPackage):
         if pkg.command:
             final_cmd = pkg.command.replace('%U', '')
-            subprocess.Popen(args=[final_cmd], shell=True, env={**os.environ})
+            subprocess.Popen(final_cmd, shell=True)
 
     def get_screenshots(self, pkg: SoftwarePackage) -> List[str]:
         pass
