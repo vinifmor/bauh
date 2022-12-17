d05013f24ee7c5d82798218b36765c0f7c143743
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2022-12-17 10:03:28 -0300

    [linux_dist/appimage] fix: AppImage build script

--- linux_dist/appimage/build.sh
+++ linux_dist/appimage/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t bauh-appimage --build-arg bauh_commit=$BAUH_COMMIT .
+docker build -t bauh-appimage --build-arg bauh_commit=$BAUH_COMMIT . && \
 docker run --cap-add=SYS_ADMIN --device /dev/fuse --mount type=bind,source="$(pwd)",target=/build bauh-appimage
