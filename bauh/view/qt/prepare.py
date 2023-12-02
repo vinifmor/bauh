5e9de37373a685436825aaecb5ba1c1b5b736a14
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2023-12-02 15:15:32 -0300

    [view.qt] enhancement: reducing the initialization panel min width

--- bauh/view/qt/prepare.py
+++ bauh/view/qt/prepare.py
@@ -291,7 +291,7 @@ class PreparePanel(QWidget, TaskManager):
         super().showEvent(event)
         self.prepare_thread.start()
         screen_size = get_current_screen_geometry()
-        self.setMinimumWidth(int(screen_size.width() * 0.5))
+        self.setMinimumWidth(int(screen_size.width() * 0.25))
         self.setMinimumHeight(int(screen_size.height() * 0.35))
         self.setMaximumHeight(int(screen_size.height() * 0.95))
         centralize(self)
