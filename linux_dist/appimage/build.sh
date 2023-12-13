708bbc07a328ed18e3630e18587aeb21ff74ea0a
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2023-12-13 10:41:37 -0300

    [linux_dist.appimage] refactoring: reducing Dockerfile size

--- linux_dist/appimage/build.sh
+++ linux_dist/appimage/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -Ceufox pipefail
 
-docker build -t bauh-appimage . && \
-docker run -e BAUH_VERSION=$BAUH_VERSION --cap-add=SYS_ADMIN --device /dev/fuse --mount type=bind,source="$(pwd)",target=/build bauh-appimage
+docker build -t bauh-appimage .
+docker run -e BAUH_VERSION=$BAUH_VERSION -v ./AppImageBuilder.yml:/build/AppImageBuilder.yml --rm --cap-add=SYS_ADMIN --device /dev/fuse --mount type=bind,source="$(pwd)",target=/build bauh-appimage
