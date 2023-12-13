2cf996c29767d14842f050cb00e4a8dbae645ea8
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2023-12-13 10:09:06 -0300

    [linux_dist/appimage] refactoring: removing 'BAUH_VERSION' envvar from the Dockerfile

--- linux_dist/appimage/build.sh
+++ linux_dist/appimage/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build -t bauh-appimage --build-arg bauh_commit=$BAUH_COMMIT . && \
-docker run --cap-add=SYS_ADMIN --device /dev/fuse --mount type=bind,source="$(pwd)",target=/build bauh-appimage
+set -Ceufox pipefail
+
+docker build -t bauh-appimage . && \
+docker run -e BAUH_VERSION=$BAUH_VERSION --cap-add=SYS_ADMIN --device /dev/fuse --mount type=bind,source="$(pwd)",target=/build bauh-appimage
